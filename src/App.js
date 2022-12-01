ed13a5a7658c23fbd52c6c46a9ece748494e0f99
Nikolaihesel <Nikolaihesel@icloud.com>
2022-12-01 20:29:33 +0100

    start opsætning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
-import {  Routes, BrowserRouter, Route } from 'react-router-dom'
+import {  Routes, BrowserRouter, Route } from 
+'react-router-dom'
+import SectionHeader from './components/sectionHeader'
 
 function App() {
   return (
     <div className="App">
 
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-     
+ 
 
-     <p className="font-thin text-4xl bg-red-500">Test</p>
-  
-     <BrowserRouter>
-     <Routes>
+    <button className="btn-prim">
+      Klik her
+    </button>
 
 
-      <Route path='/home'> Hjem </Route>
+      <SectionHeader title="oplevelser"/>
 
-     </Routes>
-     
-     </BrowserRouter>
 
 
     </div>
---
+++ src/components/sectionHeader.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../index.css';
+
+function sectionHeader(props) {
+  return (
+
+    
+    <div className="section-header">
+        
+       <h2> {props.title} </h2>
+    
+        <hr className="section-header-hr"/>
+
+
+    </div>
+
+    
+  )
+}
+
+export default sectionHeader
