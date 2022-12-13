81b15ac10e6975bb875f23f242c3666cecec7a0c
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2022-12-13 13:36:43 +0100

    Rejser
    travelsss

--- src/components/rejser.js
+++ src/components/rejser.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
+
+import Hero from '../img_mcsafari/rejse1.png';
+import rejse2 from '../img_mcsafari/rejse2.png'
+import rejse3 from '../img_mcsafari/rejse3.png'
+import rejse4 from '../img_mcsafari/rejse4.png'
+
+import Travels from './travels'
+import SubHeader from './sectionHeader';
+import ButtonPrim from './btnPrimary';
+import AboutStefan from './omStefan';
+
 
 function rejser() {
   return (
-    <div>rejser</div>
+
+
+       <div>
+        <div className="header-container"> 
+          </div> 
+    <div className="hero-subpage_test">
+      <img src={Hero} alt="" />
+          </div>
+
+      <div className=" paragraph-hero-subpage">
+         <SubHeader title="Rejser du ikke vil glemme!"/>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est maiores vero, reiciendis, optio eos suscipit ipsa et quis magni inventore tempore? Quasi quod facilis, consequuntur itaque adipisci enim architecto autem dolorum tempora libero pariatur necessitatibus atque recusandae asperiores magni similique unde quibusdam! Mollitia quas dicta amet tempore error eaque repudiandae temporibus obcaecati ut illum iste consectetur laboriosam quis.</p>
+
+        <ButtonPrim text="Læs mere" link="Offroad" />
+    </div>
+
+
+    <SubHeader title="Rejsedestinationer" />
+
+    <div className="products-container">
+
+    <Travels image={rejse2} travelName="Rwanda" link="home" textButton="Book nu" />
+    <Travels image={rejse3} travelName="Rwanda" link="home" textButton="Book nu" />
+    <Travels image={rejse4} travelName="Rwanda" link="home" textButton="Book nu" />
+    <Travels image={rejse2} travelName="Rwanda" link="home" textButton="Book nu" />
+    <Travels image={rejse3} travelName="Rwanda" link="home" textButton="Book nu" />
+    <Travels image={rejse4} travelName="Rwanda" link="home" textButton="Book nu" />
+
+
+    </div>
+
+
+    <div className="video">Video</div>
+
+    <AboutStefan />
+
+    </div>
   )
 }
 
